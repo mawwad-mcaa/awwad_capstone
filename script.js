3884073f84f32f5e39da604ef55320f535a2dd4d
SuspendedFlame <m.awwad@mcaabhm.org>
2026-05-12 15:17:57 -0500

    i combined const

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const colors = ["card-yellow", "card-red", "card-blue", "card-green"];
+const values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const cards = 'colors, values';
 
+let cardDeck = [];
+let myHand = [];
+let theirHand = [];
+
+function startgame() {
+    cardDesk = [];
+    cards.forEach((cards) => {
+        
+    });
+}
