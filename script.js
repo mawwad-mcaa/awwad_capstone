ae932fa6918cb51682a4a798693dd6eafb9603a3
SuspendedFlame <m.awwad@mcaabhm.org>
2026-05-13 08:38:17 -0500

    added consts and lets

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const colors = ["card-yellow", "card-red", "card-blue", "card-green"];
+const colors = ["yellow", "red", "blue", "green"];
 const values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const cards = 'colors, values';
 
